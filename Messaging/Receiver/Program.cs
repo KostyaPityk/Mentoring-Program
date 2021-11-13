ef4f85b97d7ba8bc5b97e753f4e591e3e9e8ec72
Konstantin Pityk <Konstantin_Pityk@epam.com>
2021-11-13 14:45:48 +0300

    Messaging

---
+++ Messaging/Receiver/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+using Microsoft.Azure.ServiceBus.Management;
+
+namespace Receiver
+{
+	class Program
+	{
+		const string connectionString = "Endpoint=sb://testmessagingmentoring.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=w+AmCBIa4UqRE5m1IziQ45eNZL0VP5xttfrfUaj1WVA=";
+		const string queueName = "messaging";
+		const string outputDirectory = "D:\\Test\\Output";
+		static IQueueClient queueClient;
+
+		static void Main(string[] args)
+		{
+			ReceiveMessages().Wait();
+		}
+
+		static async Task ReceiveMessages()
+		{
+			ManagementClient managementClient = new ManagementClient(connectionString);
+
+			bool isQueueExist = await managementClient.QueueExistsAsync(queueName);
+
+			if (!isQueueExist)
+			{
+				await managementClient.CreateQueueAsync(queueName);
+			}
+
+			queueClient = new QueueClient(connectionString, queueName, ReceiveMode.ReceiveAndDelete);
+
+
+			MessageHandlerOptions messageHandlerOptions = new MessageHandlerOptions((ExceptionReceivedEventArgs arg) => Task.FromResult(arg.Exception))
+			{
+				MaxConcurrentCalls = 1,
+				AutoComplete = false
+			};
+
+			queueClient.RegisterMessageHandler(MessageHandlerAsync, messageHandlerOptions);
+
+			Console.ReadKey();
+			await queueClient.CloseAsync();
+		}
+
+		static async Task MessageHandlerAsync(Message message, CancellationToken token)
+		{
+			string fileName = message.Label;
+			int filePosition = Convert.ToInt32(message.UserProperties["position"]);
+			string filePath = Path.Combine(outputDirectory, fileName);
+
+			using (FileStream fileStream = new FileStream(filePath, FileMode.OpenOrCreate, FileAccess.Write))
+			{
+				fileStream.Position = filePosition;
+				fileStream.Write(message.Body);
+			}
+
+			await queueClient.CompleteAsync(message.SystemProperties.LockToken);
+
+		}
+	}
+}
---
+++ Messaging/Sender/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+using System.IO;
+
+namespace Sender
+{
+	class Program
+	{
+		const string connectionString = "Endpoint=sb://testmessagingmentoring.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=w+AmCBIa4UqRE5m1IziQ45eNZL0VP5xttfrfUaj1WVA=";
+		const string queueName = "messaging";
+		const string watchDirectory = "D:\\Test\\Input";
+		const int maxMessageSize = 256000;
+
+		static IQueueClient queueClient = new QueueClient(connectionString, queueName);
+
+		static void Main(string[] args)
+		{
+			WatcherAsync().Wait();
+		}
+
+		static async Task WatcherAsync()
+		{
+			FileSystemWatcher directoryWathcer = new FileSystemWatcher(watchDirectory);
+			directoryWathcer.Created += SendMessageAsync;
+			directoryWathcer.EnableRaisingEvents = true;
+
+			ConsoleKeyInfo inputKey = Console.ReadKey();
+			if (inputKey.Key == ConsoleKey.Backspace)
+			{
+				await queueClient.CloseAsync();
+			}
+		}
+
+
+		static async void SendMessageAsync(object sender, FileSystemEventArgs e)
+		{
+			try
+			{
+				using (FileStream fileStream = new FileStream(e.FullPath, FileMode.Open))
+				{
+					while(fileStream.Position != fileStream.Length)
+					{
+						byte[] messageBody = new byte[maxMessageSize];
+
+						Message message = new Message(messageBody);
+						message.Label = e.Name;
+						message.UserProperties.Add("position", fileStream.Position);
+
+						var readBytes = fileStream.Read(messageBody, 0, maxMessageSize);
+
+						await queueClient.SendAsync(message);
+					}
+				}
+
+				Console.WriteLine("Successfully");
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine($"Exception - ${ex.Message}");
+			}
+		}
+	}
+}
